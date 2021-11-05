a0abbf4d29c1ce19f4b6af692ff58a7d3028a726
Daniel Bily <dbily@redhat.com>
2021-11-05 15:35:37 +0100

    update test cases for acks in /rule/

--- server/handlers_test.go
+++ server/handlers_test.go
@@ -897,22 +897,6 @@ func TestHTTPServer_RecommendationsListEndpoint2Rules_ImpactingMissing1RuleDisab
 			},
 		)
 
-		/*
-			ruleAcksBody := `{
-				"disabledRules":[
-					{
-						"org_id": 1,
-						"user_id": "1",
-						"rule_id": "%v",
-						"error_key": "%v",
-						"justification": "justification",
-						"created_at": %v,
-						"updated_at": %v
-					}
-				],
-				"status":"ok"
-			}`
-		*/
 		ruleAcksBody := `{
 			"disabledRules":[
 				{
