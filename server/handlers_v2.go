ccf256139be768e01431201fec50ed4446037340
Papa Bakary Camara <pcamara@redhat.com>
2022-01-18 12:15:07 +0100

    Fix getImpactedClustersFromAggregator bug

--- server/handlers_v2.go
+++ server/handlers_v2.go
@@ -778,7 +778,7 @@ func getImpactedClustersFromAggregator(
 	url string,
 	activeClusters []ctypes.ClusterName,
 ) (resp *http.Response, err error) {
-	if len(activeClusters) < 0 {
+	if len(activeClusters) < 1 { //empty array
 		// #nosec G107
 		resp, err = http.Get(url)
 		return
