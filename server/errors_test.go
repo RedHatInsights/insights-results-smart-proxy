52fe6215d51e30e61053757c7155a69c69c0855c
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-06-29 07:52:19 +0200

    Unit test for RouterMissingParamError structure

---
+++ server/errors_test.go
@@ -0,0 +1,38 @@
+// Copyright 2023 Red Hat, Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package server_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/RedHatInsights/insights-results-smart-proxy/server"
+)
+
+// TestRouterMissingParamError checks the method Error() for data structure
+// RouterMissingParamError
+func TestRouterMissingParamError(t *testing.T) {
+	// expected error value
+	const expected = "Missing required param from request: paramName"
+
+	// construct an instance of error interface
+	err := server.RouterMissingParamError{
+		ParamName: "paramName",
+	}
+
+	// check if error value is correct
+	assert.Equal(t, err.Error(), expected)
+}
