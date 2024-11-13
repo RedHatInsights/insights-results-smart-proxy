7b3c91bb31b1566e82aaacbdddf69a941bd198d4
Juan Diaz Suarez <jdiazsua@redhat.com>
2024-11-13 13:30:25 +0100

    allow user requests even if USE_RBAC is true

--- server/auth_middleware.go
+++ server/auth_middleware.go
@@ -143,6 +143,7 @@ func (server *HTTPServer) Authorization(next http.Handler, noAuthURLs []string) 
 		// be for all users, but let's first make sure we won't disturb existing users by only
 		// logging unauthorized service accounts
 		if token.Identity.Type == "ServiceAccount" {
+			log.Debug().Str("client ID", token.Identity.ServiceAccount.ClientID).Msg("Received a request from a service account")
 			// Check permissions for service accounts
 			if !server.rbacClient.IsAuthorized(auth.GetAuthTokenHeader(r)) {
 				log.Warn().Str(accountType, token.Identity.Type).Msg(accountNotAuthorized)
@@ -154,7 +155,8 @@ func (server *HTTPServer) Authorization(next http.Handler, noAuthURLs []string) 
 		} else {
 			log.Debug().Str(accountType, token.Identity.Type).Msg("RBAC is only used with service accounts for now")
 			// handleServerError(w, &auth.AuthorizationError{ErrString: "unknown identity type"})
-			return
+			// We don't use return because RBAC is not mandatory for users yet
+			// return
 		}
 
 		// Access is authorized, proceed with the request
